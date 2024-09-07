147422abe12b75005937ee2460ab51f924751860
Sam Dacara <sam@Johanns-MacBook-Air.local>
2024-09-07 15:57:41 +0800

    added login page

--- frontend/src/folds/login-fold/index.tsx
+++ frontend/src/folds/login-fold/index.tsx
@@ -1,7 +1,28 @@
 export default function LoginFold() {
-  return(
+  return (
     <section>
-      <h1 className="text-black">Login<span>.</span></h1>
+      <div className="flex flex-grow flex-col justify-center gap-[2rem] items-center">
+        <form action="" className="flex flex-col gap-[2rem] w-full max-w-[640px]">
+          <div className="flex w-full justify-start items-center">
+            <h1 className="text-black">
+              Login<em>.</em>
+            </h1>
+          </div>
+          <div className="flex flex-col gap-[1rem]">
+            <div className="flex flex-col">
+              <label htmlFor="username">Username</label>
+              <input type="text" name="username" id="username" />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" id="password" />
+            </div>
+          </div>
+          <button id="specialButton" type="submit">
+            Login
+          </button>
+        </form>
+      </div>
     </section>
   );
 }
